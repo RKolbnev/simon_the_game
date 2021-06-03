fa7e9212d9a5440998f750b66ab8a20b3991ca1c
RKolbnev <RKolbnev>
2021-06-03 18:11:23 +0300

    -

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,153 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="game">
+    <div class="simon">
+      <div class="wrap">
+        <div class="red" ref="1" @click="click(1)"></div>
+        <div class="blue" ref="2" @click="click(2)"></div>
+      </div>
+      <div class="wrap">
+        <div class="yellow" ref="3" @click="click(3)"></div>
+        <div class="green" ref="4" @click="click(4)"></div>
+      </div>
+      <button @click="start">
+        {{buttonText}}
+      </button>
+    </div>
+
+    <div class="setting">
+      <p>Раунд: {{round}}</p>
+      <p>Уровень сложности</p>
+      <label v-for="item in ['Легкий', 'Средний', 'Сложный']" :key="item">
+        <input
+          type="radio"
+          v-model="level"
+          name="level"
+          @click="time = levelTime[item]"
+          :value="item">
+        {{item}}
+      </label>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      round: 0,
+      buttonText: 'Старт',
+      inGame: false,
+      level: 'Легкий',
+      time: 1500,
+      timeoutID: null,
+      audio: {
+        1: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+        2: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+        3: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+        4: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+      },
+      levelTime: {
+        Легкий: '1500',
+        Средний: '1000',
+        Сложный: '400'
+      },
+      orderList: [],
+      userList: []
+    }
+  },
+  methods: {
+    click (id) {
+      this.audio[id].play()
+      this.$refs[id].style.opacity = '1'
+      setTimeout(() => {
+        this.$refs[id].style.opacity = '0.5'
+      }, 200)
+      if (this.inGame) {
+        clearTimeout(this.timeoutID)
+        console.log('destroy timer')
+        this.userList.push(id)
+        this.checkUserList(this.userList.length - 1)
+      }
+    },
+
+    start () {
+      this.orderList = []
+      this.userList = []
+      this.time = this.levelTime[this.level]
+      this.step()
+    },
+
+    step () {
+      this.userList = []
+      this.playOrderList()
+      this.buttonText = 'Слушайте'
+    },
+
+    playOrderList () {
+      this.inGame = false
+      this.orderList.push(this.random())
+
+      this.orderList.forEach((id, i) => {
+        setTimeout(() => {
+          this.click(id)
+        }, 700 * i)
+      })
+      setTimeout(() => {
+        this.inGame = true
+        this.buttonText = 'Повторите'
+        console.log('start timer from play')
+        this.timer()
+      }, this.orderList.length * 700)
+    },
+
+    checkUserList (item) {
+      if (this.userList[item] !== this.orderList[item]) {
+        return this.gameOver()
+      }
+      if (this.userList.length === this.orderList.length) {
+        this.round++
+        setTimeout(() => {
+          this.step()
+        }, 1000)
+      } else {
+        console.log('start timer from check')
+        this.timer()
+      }
+    },
+
+    gameOver () {
+      this.buttonText = 'Game Over'
+      setTimeout(() => {
+        this.buttonText = 'Старт'
+      }, 1000)
+      clearTimeout(this.timeoutID)
+      this.inGame = false
+      console.log(this.orderList)
+      console.log(this.userList)
+      this.userList = []
+      this.orderList = []
+    },
+
+    timer () {
+      const start = new Date().getTime()
+      this.timeoutID = setTimeout(() => {
+        const end = new Date().getTime()
+        console.log(end - start)
+        this.gameOver()
+      }, this.time)
+    },
+
+    random () {
+      const num = 1 + Math.random() * (4 + 1 - 1)
+      return Math.floor(num)
+    }
   }
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
